b53bb077acb2d65a60193e3ba112b46e3b25281d
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-02-08 20:23:08 +0200

    Do not rewrite unmodified doms

--- tools/remove_items_from_drops.py
+++ tools/remove_items_from_drops.py
@@ -19,6 +19,7 @@ def removeEmptyGroup(item_element):
 
 
 def processes_items(item_elements):
+    removedElements = []
     for item_element in item_elements:
         if item_element.get('id') in ids_to_remove:
             next_sibling = item_element.getnext()
@@ -29,14 +30,18 @@ def processes_items(item_elements):
             print('Removing item', item_element.get('id'), comment)
             item_element.getparent().remove(item_element)
             removeEmptyGroup(item_element)
+            removedElements.append(item_element)
+    return removedElements
 
 
 for item in files_to_process:
     dom = ET.parse(item)
     drop_lists = dom.findall('.//dropLists')
+    results = []
     for drop_list in drop_lists:
         item_elements = drop_list.findall('.//item')
-        processes_items(item_elements)
+        results.extend(processes_items(item_elements))
 
-    with open(item, 'wb') as f:
-        f.write(ET.tostring(dom))
+    if results:
+        with open(item, 'wb') as f:
+            f.write(ET.tostring(dom))
