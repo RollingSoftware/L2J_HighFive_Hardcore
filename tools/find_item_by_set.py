2617218d4abf277428997b2a34864bbe295f9249
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-02-12 16:38:07 +0200

    Script arguent

--- tools/find_item_by_set.py
+++ tools/find_item_by_set.py
@@ -2,7 +2,7 @@ import sys
 import file_helper as fh
 from xml.dom import minidom
 
-script_name, workdir, file_extension, recursive, attribute, value = sys.argv
+script_name, workdir, file_extension, recursive, attribute, value, item_type = sys.argv
 
 files_to_process = fh.filter_files_by_suffix(fh.files_to_process(workdir), file_extension)
 
@@ -11,12 +11,13 @@ for item in files_to_process:
     dom = minidom.parse(item)
     item_elements = dom.getElementsByTagName('item')
     for element in item_elements:
-        sets = element.getElementsByTagName('set')
-        for one_set in sets:
-            if attribute == one_set.getAttribute('name') and value == one_set.getAttribute('val'):
-                print('Item', one_set.getAttribute(attribute),
-                      '[', element.getAttribute('name'), ']')
-                item_ids.append(element.getAttribute('id'))
+        if element.getAttribute('type') in item_type.split(','):
+            sets = element.getElementsByTagName('set')
+            for one_set in sets:
+                if attribute == one_set.getAttribute('name') and one_set.getAttribute('val') in value.split(','):
+                    print('Item', one_set.getAttribute(attribute),
+                          '[', element.getAttribute('name'), ']')
+                    item_ids.append(element.getAttribute('id'))
 
 with open('find_item_by_set_result.txt', 'w') as result:
     result.write(', '.join(item_ids))
